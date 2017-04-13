67b725cd19f017dd48992cb6ec16b9087609ffc4
ngrodzitski <utromvecherom@gmail.com>
2017-04-13 14:44:14 +0300

    Add request_t

--- dev/restinio/request_handler.hpp
+++ dev/restinio/request_handler.hpp
@@ -19,6 +19,53 @@ namespace restinio
 using request_id_t = unsigned int;
 
 //
+// request_t
+//
+
+//! HTTP Request data.
+/*!
+	Provides acces to header and body, and creates response builder
+	for a given request.
+*/
+class request_t final
+	:	public std::enable_shared_from_this< request_t >
+{
+	public:
+		request_t(
+			request_id_t request_id,
+			http_request_header_t header,
+			std::string body,
+			connection_handle_t connection )
+			:	m_request_id{ request_id }
+			,	m_header{ std::move( header ) }
+			,	m_body{ std::move( body ) }
+			,	m_connection{ std::move( connection ) }
+		{}
+
+		const http_request_header_t &
+		header() const
+		{
+			return m_header;
+		}
+
+		const std::string &
+		body() const
+		{
+			return m_body;
+		}
+
+	private:
+		const request_id_t m_request_id;
+		const http_request_header_t m_header;
+		const std::string m_body;
+
+		connection_handle_t m_connection;
+};
+
+//! Request handler, that is the type for calling request handlers.
+using request_handle_t = std::shared_ptr< request_t >;
+
+//
 // http_request_t
 //
 
