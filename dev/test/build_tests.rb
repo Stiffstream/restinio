d0c5bede3d60b8e02ebae1eeb9c7bd7285e6afdc
ngrodzitski <utromvecherom@gmail.com>
2017-06-08 17:39:58 +0300

    Add escape funcs.

--- dev/test/build_tests.rb
+++ dev/test/build_tests.rb
@@ -4,6 +4,7 @@ MxxRu::Cpp::composite_target {
 	required_prj( "test/default_constructed_settings/prj.ut.rb" )
 	required_prj( "test/ref_qualifiers_settings/prj.ut.rb" )
 	required_prj( "test/response_coordinator/prj.ut.rb" )
+	required_prj( "test/escape/prj.ut.rb" )
 
 	required_prj( "test/handle_requests/method/prj.ut.rb" )
 	required_prj( "test/handle_requests/echo_body/prj.ut.rb" )
