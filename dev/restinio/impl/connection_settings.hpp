db9029485b5b5ffd8ae2e094b3b963be83f8f258
ngrodzitski <utromvecherom@gmail.com>
2017-07-26 23:08:48 +0300

    Move connection settings to separate file. Start adding ws_connection.

---
+++ dev/restinio/impl/connection_settings.hpp
@@ -0,0 +1,95 @@
+/*
+	restinio
+*/
+
+/*!
+	Connection settings.
+*/
+
+#pragma once
+
+#include <memory>
+#include <chrono>
+
+#include <nodejs/http_parser/http_parser.h>
+
+namespace restinio
+{
+
+namespace impl
+{
+
+//
+// connection_settings_t
+//
+
+//! Parameters shared between connections.
+/*!
+	Each connection has access to common params and
+	server-agent throught this object.
+*/
+template < typename TRAITS >
+struct connection_settings_t final
+	:	public std::enable_shared_from_this< connection_settings_t< TRAITS > >
+{
+	using request_handler_t = typename TRAITS::request_handler_t;
+	using logger_t = typename TRAITS::logger_t;
+
+	connection_settings_t( const connection_settings_t & ) = delete;
+	connection_settings_t( const connection_settings_t && ) = delete;
+	void operator = ( const connection_settings_t & ) = delete;
+	void operator = ( const connection_settings_t && ) = delete;
+
+	template < typename SETTINGS >
+	connection_settings_t(
+		SETTINGS & settings,
+		http_parser_settings parser_settings )
+		:	m_request_handler{ settings.request_handler() }
+		,	m_parser_settings{ parser_settings }
+		,	m_buffer_size{ settings.buffer_size() }
+		,	m_read_next_http_message_timelimit{
+				settings.read_next_http_message_timelimit() }
+		,	m_write_http_response_timelimit{
+				settings.write_http_response_timelimit() }
+		,	m_handle_request_timeout{
+				settings.handle_request_timeout() }
+		,	m_max_pipelined_requests{ settings.max_pipelined_requests() }
+		,	m_logger{ settings.logger() }
+	{}
+
+	//! Request handler factory.
+	std::unique_ptr< request_handler_t > m_request_handler;
+
+	//! Parser settings.
+	/*!
+		Parsing settings are common for each connection.
+	*/
+	const http_parser_settings m_parser_settings;
+
+	//! Params from server_settings_t.
+	//! \{
+	std::size_t m_buffer_size;
+
+	std::chrono::steady_clock::duration
+		m_read_next_http_message_timelimit{ std::chrono::seconds( 60 ) };
+
+	std::chrono::steady_clock::duration
+		m_write_http_response_timelimit{ std::chrono::seconds( 5 ) };
+
+	std::chrono::steady_clock::duration
+		m_handle_request_timeout{ std::chrono::seconds( 10 ) };
+
+	std::size_t m_max_pipelined_requests;
+
+	const std::unique_ptr< logger_t > m_logger;
+	//! \}
+};
+
+template < typename TRAITS >
+using connection_settings_shared_ptr_t =
+	std::shared_ptr< connection_settings_t< TRAITS > >;
+
+} /* namespace impl */
+
+} /* namespace restinio */
+
