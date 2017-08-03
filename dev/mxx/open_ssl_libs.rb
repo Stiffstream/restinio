98dddaefa427039ce48edcd7e373168dec366e46
ngrodzitski <utromvecherom@gmail.com>
2017-08-03 16:38:43 +0300

    OpenSSL custom lib/include satisfaction 1.
    --HG--
    branch : win-openssl-lib-selection

--- dev/mxx/open_ssl_libs.rb
+++ dev/mxx/open_ssl_libs.rb
@@ -1,43 +1,47 @@
 require 'mxx_ru/cpp'
 
-def file_exists( dir, f )
-	File.exist?( File.join( dir, f ) ) and File.file?( File.join( dir, f) )
+def file_exists?( dir, f )
+  fname = File.join( dir, f )
+  if File.exist?( fname ) and File.file?( fname )
+    fname
+  else
+    nil
+  end
 end
 
 def all_files_exist( dir, files )
-	not files.find{|f| !file_exists(dir, f ) }
+  not files.find{|f| !file_exists?(dir, f ) }
 end
 
 def find_first_openssl_libs( openssl_before_11, openssl_after_11 )
 
-	lib_dirs = ENV[ "LIB" ]
-	if lib_dirs
+  lib_dirs = ENV[ "LIB" ]
+  if lib_dirs
 
-		dirs_to_look = lib_dirs.split( ';' ).select{|p| p != "" }
-		puts dirs_to_look, ""
+    dirs_to_look = lib_dirs.split( ';' ).select{|p| p != "" }
 
-		dir_indx_before_11 = dirs_to_look.find_index{|d| all_files_exist( d, openssl_before_11 ) }
-		dir_indx_after_11 = dirs_to_look.find_index{|d| all_files_exist( d, openssl_after_11 ) }
-	end
+    dir_indx_before_11 = dirs_to_look.find_index{|d| all_files_exist( d, openssl_before_11 ) }
+    dir_indx_after_11 = dirs_to_look.find_index{|d| all_files_exist( d, openssl_after_11 ) }
+  end
 
-	if dir_indx_before_11 and dir_indx_after_11 
-		if dir_indx_after_11 < dir_indx_after_11 
-			openssl_before_11
-		else
-			openssl_after_11
-		end
-	elsif dir_indx_before_11 
-		openssl_before_11
-	elsif dir_indx_after_11 
-		openssl_after_11
-	else
-		raise "OpenSSL libs not found; tried [#{openssl_before_11.join(', ')}] and [#{openssl_after_11.join(', ')}]"
-	end
+  if dir_indx_before_11 and dir_indx_after_11
+    if dir_indx_after_11 < dir_indx_after_11
+      openssl_before_11
+    else
+      openssl_after_11
+    end
+  elsif dir_indx_before_11
+    openssl_before_11
+  elsif dir_indx_after_11
+    openssl_after_11
+  else
+    raise "OpenSSL libs not found; tried [#{openssl_before_11.join(', ')}] and [#{openssl_after_11.join(', ')}]"
+  end
 end
 
 def get_libs_vc
-  find_first_openssl_libs( 
-    [ 'libeay32.lib', 'ssleay32.lib' ], 
+  find_first_openssl_libs(
+    [ 'libeay32.lib', 'ssleay32.lib' ],
     [ 'libssl.lib', 'libcrypto.lib' ] )
 end
 
@@ -51,7 +55,7 @@ end
 
 
 def get_libs
-  if 'mswin' == toolset.tag( 'target_os' ) 
+  if 'mswin' == toolset.tag( 'target_os' )
     if 'vc' == toolset.name
       get_libs_vc
     elsif 'gcc' == toolset.name
@@ -63,12 +67,17 @@ def get_libs
 end
 
 MxxRu::Cpp::lib_collection_target {
-  get_libs.each{|l| lib(l)}
+  custom = file_exists?( "", "../local-openssl-dependency.rb" )
+  if custom
+    eval File.read( 'local-openssl-dependency.rb' )
+  else
+    get_libs.each{|l| lib(l)}
+  end
 }
 
 if __FILE__ == $0
-	libs = get_libs_mingw
-	if libs 
+	libs = get_libs
+	if libs
 		puts "Openssl libs: #{libs.join(', ')}"
 	else
 		puts "Openssl libs not found"
