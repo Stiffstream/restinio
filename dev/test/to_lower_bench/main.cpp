bdb133526a50da1849948cd913d8cf8b4c601a06
ngrodzitski <utromvecherom@gmail.com>
2017-08-24 11:20:54 +0300

    Reafactoring tolower bench.
    --HG--
    branch : websocket-draft

--- dev/test/to_lower_bench/main.cpp
+++ dev/test/to_lower_bench/main.cpp
@@ -182,97 +182,130 @@ by_char_group_8x_caseless_cmp(
 	return true;
 }
 
+constexpr unsigned char to_lower_lut[] = {
+0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
+0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+0x40,
+	  0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
+0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A,
+																  0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
+0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
+0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F,
+0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
+0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F,
+0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
+0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF,
+0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
+0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF,
+0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF,
+0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF };
+
+bool
+by_lut1_caseless_cmp(
+	const char * a,
+	const char * b,
+	std::size_t size )
+{
+	for( std::size_t i = 0; i < size; ++i )
+		if( to_lower_lut[ (unsigned char)a[ i ] ] != to_lower_lut[ (unsigned char)b[ i ] ] )
+			return false;
+
+	return true;
+}
+
+bool
+by_lut2_caseless_cmp(
+	const char * a,
+	const char * b,
+	std::size_t size )
+{
+	auto n = size & ( 0xFFFFFFFF - 0x1 );
+
+	std::size_t i = 0;
+	while( i < n )
+	{
+		char c1,c2;
+
+		c1 = to_lower_lut[ (unsigned char)a[i]];
+		c2 = to_lower_lut[ (unsigned char)b[i]];
+		if( c1 != c2 ) return false;
+
+		c1 = to_lower_lut[ (unsigned char)a[i + 1]];
+		c2 = to_lower_lut[ (unsigned char)b[i + 1]];
+		if( c1 != c2 ) return false;
+
+		i+=2;
+	}
+
+	for( ; i < size; ++i )
+	{
+		if( tolower( a[ i ] ) != tolower( b[ i ] ) )
+			return false;
+	}
+
+	return true;
+}
+
+
+
+inline bool
+by_lut3_caseless_cmp(
+	const char * a,
+	const char * b,
+	std::size_t size )
+{
+	auto n = size & (0xFFFFFFFF - 0x3 );
+
+	std::size_t i = 0;
+	for( ; i < n; i+=4 )
+	{
+		alignas( alignof(std::uint32_t) )
+		unsigned char c[] = {
+			to_lower_lut[ (unsigned char)a[ i ] ],
+			to_lower_lut[ (unsigned char)a[ i + 1 ] ],
+			to_lower_lut[ (unsigned char)a[ i + 2 ] ],
+			to_lower_lut[ (unsigned char)a[ i + 3 ] ],
+			to_lower_lut[ (unsigned char)b[ i ] ],
+			to_lower_lut[ (unsigned char)b[ i + 1 ] ],
+			to_lower_lut[ (unsigned char)b[ i + 2 ] ],
+			to_lower_lut[ (unsigned char)b[ i + 3 ] ] };
+
+		if( *(std::uint32_t*)c != *(std::uint32_t*)(c + 4) )
+			return false;
+	}
+
+	for( ; i < size; ++i )
+	{
+		if( to_lower_lut[ (unsigned char)a[ i ] ] != to_lower_lut[ (unsigned char)b[ i ] ] )
+			return false;
+	}
+
+	return true;
+}
+
 const std::size_t iterations_count = 1000 * 1000;
 
-void
-std_bench(
-	const std::vector< std::string > & s1,
-	const std::vector< std::string > & s2 )
-{
-	for( std::size_t i = 0; i < iterations_count; ++i )
-	{
-		for( std::size_t n = 0; n < s1.size(); ++n )
-		{
-			if( !std_caseless_cmp( s1[ n ].data(), s2[ n ].data(), s1[ n ].size() ) )
-				throw std::runtime_error{ "MUST NEVER HAPPEN" };
-		}
+#define TOLOWER_BENCH( bench_func, cmp_func ) \
+	void bench_func ( const std::vector< std::string > & s1, const std::vector< std::string > & s2 ){ \
+		for( std::size_t i = 0; i < iterations_count; ++i ) { \
+			for( std::size_t n = 0; n < s1.size(); ++n ) { \
+				if( !cmp_func( s1[ n ].data(), s2[ n ].data(), s1[ n ].size() ) ) \
+					throw std::runtime_error{ "MUST NEVER HAPPEN" }; \
+			} \
+		} \
 	}
-}
 
-void
-by_char_bench(
-	const std::vector< std::string > & s1,
-	const std::vector< std::string > & s2 )
-{
-	for( std::size_t i = 0; i < iterations_count; ++i )
-	{
-		for( std::size_t n = 0; n < s1.size(); ++n )
-		{
-			if( !by_char_caseless_cmp( s1[ n ].data(), s2[ n ].data(), s1[ n ].size() ) )
-				throw std::runtime_error{ "MUST NEVER HAPPEN" };
-		}
-	}
-}
-
-void
-by_4char_bench(
-	const std::vector< std::string > & s1,
-	const std::vector< std::string > & s2 )
-{
-	for( std::size_t i = 0; i < iterations_count; ++i )
-	{
-		for( std::size_t n = 0; n < s1.size(); ++n )
-		{
-			if( !by_4char_caseless_cmp( s1[ n ].data(), s2[ n ].data(), s1[ n ].size() ) )
-				throw std::runtime_error{ "MUST NEVER HAPPEN" };
-		}
-	}
-}
-
-void
-by_char_group_2x_bench(
-	const std::vector< std::string > & s1,
-	const std::vector< std::string > & s2 )
-{
-	for( std::size_t i = 0; i < iterations_count; ++i )
-	{
-		for( std::size_t n = 0; n < s1.size(); ++n )
-		{
-			if( !by_char_group_2x_caseless_cmp( s1[ n ].data(), s2[ n ].data(), s1[ n ].size() ) )
-				throw std::runtime_error{ "MUST NEVER HAPPEN" };
-		}
-	}
-}
-
-void
-by_char_group_4x_bench(
-	const std::vector< std::string > & s1,
-	const std::vector< std::string > & s2 )
-{
-	for( std::size_t i = 0; i < iterations_count; ++i )
-	{
-		for( std::size_t n = 0; n < s1.size(); ++n )
-		{
-			if( !by_char_group_4x_caseless_cmp( s1[ n ].data(), s2[ n ].data(), s1[ n ].size() ) )
-				throw std::runtime_error{ "MUST NEVER HAPPEN" };
-		}
-	}
-}
-
-void
-by_char_group_8x_bench(
-	const std::vector< std::string > & s1,
-	const std::vector< std::string > & s2 )
-{
-	for( std::size_t i = 0; i < iterations_count; ++i )
-	{
-		for( std::size_t n = 0; n < s1.size(); ++n )
-		{
-			if( !by_char_group_8x_caseless_cmp( s1[ n ].data(), s2[ n ].data(), s1[ n ].size() ) )
-				throw std::runtime_error{ "MUST NEVER HAPPEN" };
-		}
-	}
-}
+TOLOWER_BENCH( std_bench, std_caseless_cmp )
+TOLOWER_BENCH( by_char_bench, by_char_caseless_cmp )
+TOLOWER_BENCH( by_4char_bench, by_4char_caseless_cmp )
+TOLOWER_BENCH( by_char_group_2x_bench, by_char_group_2x_caseless_cmp )
+TOLOWER_BENCH( by_char_group_4x_bench, by_char_group_4x_caseless_cmp )
+TOLOWER_BENCH( by_char_group_8x_bench, by_char_group_8x_caseless_cmp )
+TOLOWER_BENCH( by_lut1_bench, by_lut1_caseless_cmp )
+TOLOWER_BENCH( by_lut2_bench, by_lut2_caseless_cmp )
+TOLOWER_BENCH( by_lut3_bench, by_lut3_caseless_cmp )
 
 std::vector< std::string >
 create_test_strings();
@@ -298,6 +331,11 @@ run_bench( const std::string & tag, LAMBDA lambda )
 	}
 }
 
+#define RUN_TOLOWER_BENCH( bench_func, tag ) \
+	run_bench( \
+		tag, \
+		[&]{ bench_func( str_set_1, str_set_2 ); } );
+
 int
 main()
 {
@@ -306,42 +344,23 @@ main()
 	const auto str_set_1 = create_test_strings();
 	const auto str_set_2 = create_test_strings();
 
-	run_bench(
-		"std_tolower",
-		[&]{
-			std_bench( str_set_1, str_set_2 );
-		} );
+	RUN_TOLOWER_BENCH( std_bench, "std_tolower" );
 
-	run_bench(
-		"by_char",
-		[&]{
-			by_char_bench( str_set_1, str_set_2 );
-		} );
+	RUN_TOLOWER_BENCH( by_char_bench, "by_char" );
 
-	run_bench(
-		"by_4char",
-		[&]{
-			by_4char_bench( str_set_1, str_set_2 );
-		} );
+	RUN_TOLOWER_BENCH( by_4char_bench, "by_4char" );
 
-	run_bench(
-		"by_char_group_2x",
-		[&]{
-			by_char_group_2x_bench( str_set_1, str_set_2 );
-		} );
+	RUN_TOLOWER_BENCH( by_char_group_2x_bench, "by_char_group_2x" );
 
-	run_bench(
-		"by_char_group_4x",
-		[&]{
-			by_char_group_4x_bench( str_set_1, str_set_2 );
-		} );
+	RUN_TOLOWER_BENCH( by_char_group_4x_bench, "by_char_group_4x" );
 
+	RUN_TOLOWER_BENCH( by_char_group_8x_bench, "by_char_group_8x" );
 
-	run_bench(
-		"by_char_group_8x",
-		[&]{
-			by_char_group_8x_bench( str_set_1, str_set_2 );
-		} );
+	RUN_TOLOWER_BENCH( by_lut1_bench, "by_lut1" );
+
+	RUN_TOLOWER_BENCH( by_lut2_bench, "by_lut2" );
+
+	RUN_TOLOWER_BENCH( by_lut3_bench, "by_lut3" );
 
 
 	return 0;
