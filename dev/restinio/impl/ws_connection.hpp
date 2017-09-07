35a20b280db5497661906db18eb08c64596cf39a
ngrodzitski <utromvecherom@gmail.com>
2017-09-07 11:58:34 +0300

    Minor refactoring.
    --HG--
    branch : websocket-draft

--- dev/restinio/impl/ws_connection.hpp
+++ dev/restinio/impl/ws_connection.hpp
@@ -187,8 +187,8 @@ class ws_connection_t final
 
 		ws_connection_t( const ws_connection_t & ) = delete;
 		ws_connection_t( ws_connection_t && ) = delete;
-		void operator = ( const ws_connection_t & ) = delete;
-		void operator = ( ws_connection_t && ) = delete;
+		const ws_connection_t & operator = ( const ws_connection_t & ) = delete;
+		ws_connection_t & operator = ( ws_connection_t && ) = delete;
 
 		~ws_connection_t()
 		{
