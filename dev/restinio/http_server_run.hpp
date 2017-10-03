141c146bc15546596a9b60eea95b3b6eb4856c8d
eao197 <eao197@gmail.com>
2017-10-03 16:03:16 +0300

    Version of run for thread pool.
    --HG--
    branch : websocket-draft

--- dev/restinio/http_server_run.hpp
+++ dev/restinio/http_server_run.hpp
@@ -40,6 +40,47 @@ run_on_this_thread_settings_t<Traits>
 on_this_thread() { return run_on_this_thread_settings_t<Traits>{}; }
 
 //
+// run_on_thread_pool_settings_t
+//
+//FIXME: document this!
+template<typename Traits>
+class run_on_thread_pool_settings_t final
+	:	public basic_server_settings_t<
+				run_on_thread_pool_settings_t<Traits>,
+				Traits>
+{
+	using base_type_t = basic_server_settings_t<
+				run_on_thread_pool_settings_t<Traits>, Traits>;
+
+	//! Size of the pool.
+	std::size_t m_pool_size;
+
+public:
+	//! Constructor.
+	run_on_thread_pool_settings_t(
+		//! Size of the pool.
+		std::size_t pool_size )
+		:	m_pool_size(pool_size)
+	{}
+
+	std::size_t
+	pool_size() const { return m_pool_size; }
+};
+
+//
+// on_thread_pool
+//
+//FIXME: document this!
+template<typename Traits = default_traits_t>
+run_on_thread_pool_settings_t<Traits>
+on_thread_pool(
+	//! Size of the pool.
+	std::size_t pool_size )
+{
+	return run_on_thread_pool_settings_t<Traits>( pool_size );
+}
+
+//
 // run()
 //
 
@@ -78,27 +119,33 @@ run( run_on_this_thread_settings_t<Traits> && settings )
 	io_context.run();
 }
 
-#if 0
-	else
-	{
-		server_t server{
-			restinio::create_child_io_context( n ),
-			std::move( s ) };
+//! Helper function for running http server until ctrl+c is hit.
+template<typename Traits>
+inline void
+run( run_on_thread_pool_settings_t<Traits> && settings )
+{
+	using settings_t = run_on_thread_pool_settings_t<Traits>;
+	using server_t = http_server_t<Traits>;
 
-		std::promise< void > promise;
+	const auto pool_size = settings.pool_size();
 
-		asio::signal_set break_signals{ server.io_context(), SIGINT };
-		break_signals.async_wait(
-			[&]( const asio::error_code & ec, int ){
-				if( !ec )
-					promise.set_value();
-			} );
+	server_t server{
+		restinio::create_child_io_context( pool_size ),
+		std::forward<settings_t>(settings) };
 
-		server.open();
-		promise.get_future().get();
-		server.close();
-	}
-#endif
+	std::promise< void > promise;
+
+	asio::signal_set break_signals{ server.io_context(), SIGINT };
+	break_signals.async_wait(
+		[&]( const asio::error_code & ec, int ){
+			if( !ec )
+				promise.set_value();
+		} );
+
+	server.open();
+	promise.get_future().get();
+	server.close();
+}
 
 } /* namespace restinio */
 
