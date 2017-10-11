a99fa634e9a5ec1793fe958137f76265a8caf466
hg@nshmakov <hg@nshmakov>
2017-10-11 22:35:11 +0300

    ws_registry added to detailed websocket sample.
    --HG--
    branch : websocket-draft

--- dev/sample/websocket_detailed/main.cpp
+++ dev/sample/websocket_detailed/main.cpp
@@ -25,8 +25,15 @@ using traits_t =
 
 using http_server_t = restinio::http_server_t< traits_t >;
 
+using ws_registry_t = std::map< std::uint64_t, rws::ws_handle_t >;
+
 struct ws_message_handler_t
 {
+	ws_message_handler_t( ws_registry_t & registry )
+	:	m_registry{ registry }
+	{
+	}
+
 	void
 	operator()( rws::ws_handle_t wsh, rws::message_handle_t m )
 	{
@@ -51,6 +58,8 @@ struct ws_message_handler_t
 		{
 			wsh->send_message( *m );
 			wsh->shutdown();
+
+			m_registry.erase( wsh->connection_id() );
 		}
 		else
 		{
@@ -64,25 +73,29 @@ struct ws_message_handler_t
 		}
 	}
 
+	ws_registry_t & m_registry;
+
 	rws::opcode_t m_continued_frame_opcode;
 	std::string m_continuation_frame_buffer;
 };
 
-auto server_handler( rws::ws_handle_t & websocket )
+auto server_handler( ws_registry_t & registry )
 {
 	auto router = std::make_unique< router_t >();
 
 	router->http_get(
 		"/",
-		[&]( auto req, auto ){
+		[&registry]( auto req, auto ){
 
 			if( restinio::http_connection_header_t::upgrade == req->header().connection() )
 			{
-				websocket =
+				auto wsh =
 					rws::upgrade< traits_t >(
 						*req,
 						rws::activation_t::immediate,
-						ws_message_handler_t{} );
+						ws_message_handler_t{registry} );
+
+				registry.emplace( wsh->connection_id(), wsh );
 
 				return restinio::request_accepted();
 			}
@@ -99,15 +112,17 @@ int main()
 
 	try
 	{
-		rws::ws_handle_t websocket;
+		ws_registry_t registry;
 
-		run( restinio::on_this_thread<traits_t>()
-					.address( "localhost" )
-					.port( 9001 )
-					.request_handler( server_handler( websocket ) )
-					.read_next_http_message_timelimit( 10s )
-					.write_http_response_timelimit( 1s )
-					.handle_request_timeout( 1s ) );
+		restinio::run(
+			restinio::on_this_thread<traits_t>()
+				.address( "localhost" )
+				.port( 9001 )
+				.request_handler( server_handler( registry ) )
+				.read_next_http_message_timelimit( 10s )
+				.write_http_response_timelimit( 1s )
+				.handle_request_timeout( 1s )
+				.cleanup_func( [&]{ registry.clear(); } ) );
 
 	}
 	catch( const std::exception & ex )
