252756faeff167e8486e4474577075bbd311f12f
ngrodzitski <utromvecherom@gmail.com>
2017-10-11 18:28:00 +0300

    Add comments onm API.
    --HG--
    branch : websocket-draft

--- dev/restinio/request_handler.hpp
+++ dev/restinio/request_handler.hpp
@@ -55,19 +55,26 @@ class request_t final
 			,	m_connection{ std::move( connection ) }
 		{}
 
+		//! Get request header.
 		const http_request_header_t &
 		header() const
 		{
 			return m_header;
 		}
 
+		//! Get request body.
 		const std::string &
 		body() const
 		{
 			return m_body;
 		}
 
-		template < typename Response_Builder = restinio_controlled_output_t >
+		//! Create response.
+		/*!
+			Creates response object if is called for the first time
+			all further calls will throw exception.
+		*/
+		template < typename Output = restinio_controlled_output_t >
 		auto
 		create_response(
 			std::uint16_t status_code = 200,
@@ -75,7 +82,7 @@ class request_t final
 		{
 			check_connection();
 
-			return response_builder_t< Response_Builder >{
+			return response_builder_t< Output >{
 				status_code,
 				reason_phrase,
 				std::move( m_connection ),
