d81d0dfa85138a29c6e3150e2418739e09eba939
ngrodzitski <utromvecherom@gmail.com>
2017-11-02 22:36:46 +0300

    Update asio.

--- externals.rb
+++ externals.rb
@@ -7,7 +7,7 @@ end
 
 MxxRu::git_externals :asio do |e|
   e.url 'https://github.com/chriskohlhoff/asio.git'
-  e.commit 'f5c570826d2ebf50eb38c44039181946a473148b'
+  e.commit 'd52b8164665e779f58e30d60e4ff9a8da1ce3c4a'
 
   e.map_dir 'asio/include' => 'dev/asio'
 end
