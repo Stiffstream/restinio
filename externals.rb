42e4413eb5e98fa51e7af7c11f4b9ac2aba71b45
ngrodzitski <utromvecherom@gmail.com>
2017-11-14 13:42:51 +0300

    Update Sobjectizer.

--- externals.rb
+++ externals.rb
@@ -1,5 +1,5 @@
 MxxRu::arch_externals :so5 do |e|
-  e.url 'https://sourceforge.net/projects/sobjectizer/files/sobjectizer/SObjectizer%20Core%20v.5.5/so-5.5.19.3.tar.xz'
+  e.url 'https://sourceforge.net/projects/sobjectizer/files/sobjectizer/SObjectizer%20Core%20v.5.5/so-5.5.19.5.tar.xz'
 
   e.map_dir 'dev/so_5' => 'dev'
   e.map_dir 'dev/timertt' => 'dev'
