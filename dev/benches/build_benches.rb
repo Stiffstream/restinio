8b830b78d9ad517331a5a564768f5d6d137596a4
ngrodzitski <utromvecherom@gmail.com>
2017-11-14 13:34:42 +0300

    Remove tick timer.

--- dev/benches/build_benches.rb
+++ dev/benches/build_benches.rb
@@ -3,7 +3,6 @@ require 'mxx_ru/cpp'
 
 MxxRu::Cpp::composite_target {
 	required_prj "benches/single_handler/prj.rb"
-	required_prj "benches/single_handler_tick_timer/prj.rb"
 	required_prj "benches/single_handler_so5_timer/prj.rb"
 	required_prj "benches/single_handler_no_timer/prj.rb"
 }
