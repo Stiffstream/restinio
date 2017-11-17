7165da208f17a2612ac7a6a3afcd02f30467632a
ngrodzitski <utromvecherom@gmail.com>
2017-11-17 11:40:11 +0300

    Add cmp hardcoded router.

--- dev/test/router/cmp_router_bench/prj.rb
+++ dev/test/router/cmp_router_bench/prj.rb
@@ -6,7 +6,7 @@ MxxRu::Cpp::exe_target {
 	required_prj 'fmt_mxxru/prj.rb'
 	required_prj 'restinio/platform_specific_libs.rb'
 
-	target( "_test.router.express_router_bench" )
+	target( "_test.router.cmp_router_bench" )
 
 	cpp_source( "route_parser.cpp" )
 	cpp_source( "main.cpp" )
