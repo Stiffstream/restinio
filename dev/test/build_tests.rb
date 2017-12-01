9a3383d9c6b487d5cf224eab06eeabbbf09d77e9
ngrodzitski <utromvecherom@gmail.com>
2017-12-01 19:55:28 +0300

    Add unit test for express router stuff for PCRE engine.

--- dev/test/build_tests.rb
+++ dev/test/build_tests.rb
@@ -35,6 +35,7 @@ MxxRu::Cpp::composite_target {
 	required_prj( "test/http_pipelining/timeouts/prj.ut.rb" )
 
 	required_prj( "test/router/express/prj.ut.rb" )
+	required_prj( "test/router/express_pcre/prj.ut.rb" )
 	required_prj( "test/router/express_router/prj.ut.rb" )
 	required_prj( "test/router/express_router_bench/prj.rb" )
 	required_prj( "test/router/cmp_router_bench/prj.rb" )
