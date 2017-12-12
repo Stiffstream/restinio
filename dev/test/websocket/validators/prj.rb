c964aa65b1ab81821df5a8421803adc06a7fafe3
ngrodzitski <utromvecherom@gmail.com>
2017-12-13 00:41:10 +0300

    Fix typo.

--- dev/test/websocket/validators/prj.rb
+++ dev/test/websocket/validators/prj.rb
@@ -6,7 +6,7 @@ MxxRu::Cpp::exe_target {
 	required_prj 'fmt_mxxru/prj.rb'
 	required_prj 'restinio/platform_specific_libs.rb'
 
-	target( "_unit.test.websocket.validaors" )
+	target( "_unit.test.websocket.validators" )
 
 	cpp_source( "main.cpp" )
 }
