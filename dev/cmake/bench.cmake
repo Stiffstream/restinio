19fde5a9f79d87fe2f0e5ffea3daf82a8fd38f8e
ngrodzitski <utromvecherom@gmail.com>
2018-01-06 04:11:52 +0300

    Simplify cmake files for benches.

---
+++ dev/cmake/bench.cmake
@@ -0,0 +1,21 @@
+if(NOT BENCH)
+	message(FATAL_ERROR "BENCH is not defined!")
+endif()
+
+if(NOT BENCH_SRCFILES)
+	set(BENCH_SRCFILES main.cpp)
+endif()
+
+add_executable(${BENCH} ${BENCH_SRCFILES})
+
+target_link_libraries(${BENCH} nodejs_http_parser)
+
+if(WIN32)
+	target_link_libraries(${BENCH} wsock32 ws2_32)
+endif()
+
+if( NOT (RESTINIO_USE_BOOST_ASIO STREQUAL "none") )
+	target_link_libraries(${BENCH} ${Boost_LIBRARIES} )
+endif()
+
+install(TARGETS ${BENCH} DESTINATION bin)
