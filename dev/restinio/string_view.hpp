bc98ecb4fbb6a5d4a3b178069245dc63a701fca9
ngrodzitski <utromvecherom@gmail.com>
2018-03-30 00:03:53 +0300

    Rely on third_party sv, which itself uses std::string_view if possible.

--- dev/restinio/string_view.hpp
+++ dev/restinio/string_view.hpp
@@ -8,49 +8,9 @@
 
 #pragma once
 
-#if defined( __has_include )
-	//
-	// Check for std::string_view or std::experimental::string_view
-	//
-	#define RESTINIO_CHECK_FOR_STRING_VIEW
-	#if defined( _MSC_VER ) && !_HAS_CXX17
-		// Visual C++ 14.* allows to include <string_view> only in c++17 mode.
-		#undef RESTINIO_CHECK_FOR_STRING_VIEW
-	#endif
-
-	#if defined( RESTINIO_CHECK_FOR_STRING_VIEW )
-
-		#if __has_include(<experimental/string_view>)
-			#include <experimental/string_view>
-			#define RESTINIO_HAS_EXPERIMENTAL_STRING_VIEW
-		#elif __has_include(<string_view>)
-			#include <string_view>
-			#define RESTINIO_HAS_STRING_VIEW
-		#endif
-
-		#if defined( RESTINIO_HAS_STRING_VIEW ) || defined( RESTINIO_HAS_EXPERIMENTAL_STRING_VIEW )
-			#define RESTINIO_SUPPORTS_STRING_VIEW
-		#endif
-	#endif
-#endif
-
-#if defined( RESTINIO_SUPPORTS_STRING_VIEW )
-namespace restinio
-{
-	#if defined( RESTINIO_HAS_STRING_VIEW )
-		using string_view_t = std::string_view;
-	#elif defined( RESTINIO_HAS_EXPERIMENTAL_STRING_VIEW )
-		using string_view_t = std::experimental::string_view;
-	#endif
-} /* namespace restinio */
-
-#else
-
 #include "third_party/string-view-lite/string_view.hpp"
 
 namespace restinio
 {
 	using string_view_t = nonstd::string_view;
 } /* namespace restinio */
-
-#endif
