d8c0f1653b3a03168b6f34b6a3351ceb38baae24
ngrodzitski <utromvecherom@gmail.com>
2018-04-29 13:07:15 +0300

    Formatting.

--- dev/cmake/bench.cmake
+++ dev/cmake/bench.cmake
@@ -1,18 +1,18 @@
-if(NOT BENCH)
+IF (NOT BENCH)
 	message(FATAL_ERROR "BENCH is not defined!")
-endif()
+ENDIF ()
 
-if(NOT BENCH_SRCFILES)
-	set(BENCH_SRCFILES main.cpp)
-endif()
+IF (NOT BENCH_SRCFILES)
+	SET(BENCH_SRCFILES main.cpp)
+ENDIF ()
 
 add_executable(${BENCH} ${BENCH_SRCFILES})
 
 TARGET_LINK_LIBRARIES(${BENCH} PRIVATE restinio::restinio)
 
-if(WIN32)
+IF (WIN32)
 	TARGET_LINK_LIBRARIES(${BENCH} PRIVATE wsock32 ws2_32)
-endif()
+ENDIF ()
 
 IF (RESTINIO_INSTALL_BENCHES)
 	install(TARGETS ${BENCH} DESTINATION bin)
