dcc01b961710139b8d8f02e5ba7aadb3ac8cfc06
ngrodzitski <utromvecherom@gmail.com>
2018-06-12 13:55:32 +0300

    Make fmtlib buildable with cmake in restinio build-tree.

--- externals.rb
+++ externals.rb
@@ -33,6 +33,7 @@ MxxRu::arch_externals :fmt do |e|
   e.url 'https://github.com/fmtlib/fmt/archive/5.0.0.zip'
 
   e.map_dir 'include' => 'dev/fmt'
+  e.map_dir 'src' => 'dev/fmt'
   e.map_dir 'support' => 'dev/fmt'
   e.map_file 'CMakeLists.txt' => 'dev/fmt/*'
   e.map_file 'README.rst' => 'dev/fmt/*'
