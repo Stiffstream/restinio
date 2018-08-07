f20d5ec13bb79fe12513e3781472a546f533025a
ngrodzitski <utromvecherom@gmail.com>
2018-08-07 10:14:39 +0300

    Implement another idea for notificator sample.

--- dev/sample/notificators/main.cpp
+++ dev/sample/notificators/main.cpp
@@ -2,7 +2,54 @@
 
 #include <restinio/all.hpp>
 
-std::atomic< int > g_failed_responses{ 0 };
+using resp_parts_container_t = std::vector< std::string >;
+
+resp_parts_container_t g_response_parts{
+	R"-STR-(<!doctype html>
+	<html>)-STR-",
+	R"-STR-(
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta name="author" content="RESTinio">
+	<meta name="description" content="RESTinio notificators">
+	<title>RESTinio notificators sample</title>
+	<style type="text/css">body {font-size: 1.1em; line-height: 1.5em; max-width: 45em; margin: auto; padding: 0 2%;} img {max-width: 100%; display: block; margin: .75em auto;}</style>
+</head>)-STR-",
+	R"-STR-(
+<body>
+	<p>This page is provided by RESTinio sample using notificators.</p>
+</body>)-STR-",
+	R"-STR-(
+</html>)-STR-" };
+
+
+using resp_builder_t = restinio::response_builder_t< restinio::chunked_output_t >;
+using resp_builder_shared_ptr_t = std::shared_ptr< resp_builder_t >;
+
+void
+stream_response_chunks(
+	resp_builder_shared_ptr_t resp,
+	resp_parts_container_t::const_iterator it )
+{
+	if( end( g_response_parts ) == it )
+	{
+		resp->done();
+	}
+	else
+	{
+		resp->append_chunk( restinio::const_buffer( it->data(), it->size() ) );
+
+		resp->flush( [ r = resp,
+				it = ++it ]( const restinio::asio_ns::error_code & ec ) mutable {
+
+				if( !ec )
+				{
+					stream_response_chunks( std::move( r ), it );
+				}
+			} );
+	}
+}
 
 // Create request handler.
 restinio::request_handling_status_t handler( restinio::request_handle_t req )
@@ -12,22 +59,16 @@ restinio::request_handling_status_t handler( restinio::request_handle_t req )
 	{
 		std::thread handler_thread{ [ req = std::move( req ) ]{
 
-			// Simulate some delay.
-			std::this_thread::sleep_for( std::chrono::seconds( 5 ) );
-
-			req->create_response()
-				.append_header( restinio::http_field::server, "RESTinio hello world server" )
+			auto resp = req->create_response< restinio::chunked_output_t >()
+				.append_header( restinio::http_field::server, "RESTinio notificators sample" )
 				.append_header_date_field()
-				.append_header( restinio::http_field::content_type, "text/plain; charset=utf-8" )
-				.set_body( "Notificators: Hello world !")
-				.done( []( const auto & ec ){
-					if( ec )
-					{
-						// Increment the number of failed requests.
-						++g_failed_responses;
+				.append_header(
+					restinio::http_field::content_type,
+					"text/html; charset=utf-8" );
 
-					}
-				} );
+			stream_response_chunks(
+				std::make_shared< resp_builder_t >( std::move( resp ) ),
+				begin( g_response_parts ) );
 		} };
 
 		handler_thread.detach();
@@ -42,37 +83,12 @@ int main()
 {
 	try
 	{
-		std::thread failed_responses_printer{
-			[]{
-				int last_printed_failed_responses{ 0 };
-
-				while( true )
-				{
-					std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
-
-					if( -1 == g_failed_responses ) return;
-
-					if( last_printed_failed_responses < g_failed_responses )
-					{
-						last_printed_failed_responses = g_failed_responses;
-
-						std::cout << "Failed responses count: "
-							<< last_printed_failed_responses << std::endl;
-					}
-				}
-			} };
-
 		restinio::run(
 			restinio::on_thread_pool( std::thread::hardware_concurrency() )
 				.port( 8080 )
 				.address( "localhost" )
 				.max_pipelined_requests( 4 )
 				.request_handler( handler ) );
-
-		std::cout << "Final failed responses count: " << g_failed_responses << std::endl;
-
-		g_failed_responses = -1;
-		failed_responses_printer.join();
 	}
 	catch( const std::exception & ex )
 	{
