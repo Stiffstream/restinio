88bf067b94ebbc91987238d57e79352c1ff5b7d3
eao197 <eao197@gmail.com>
2019-01-17 15:43:20 +0300

    Switch to SObjectizer-5.5.24.1.

--- externals.rb
+++ externals.rb
@@ -1,5 +1,5 @@
 MxxRu::arch_externals :so5 do |e|
-  e.url 'https://github.com/eao197/so-5-5/archive/v5.5.23.tar.gz'
+  e.url 'https://github.com/eao197/so-5-5/archive/v5.5.24.1.tar.gz'
   e.map_dir 'dev/so_5' => 'dev'
   e.map_dir 'dev/timertt' => 'dev'
 end
