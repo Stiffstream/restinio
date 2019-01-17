34e0199419cd00021af215f61b86b28373a76fab
eao197 <eao197@gmail.com>
2019-01-17 17:15:47 +0300

    Attempt to add a simple implementation of remote_endpoint to request_t.

--- dev/restinio/request_handler.hpp
+++ dev/restinio/request_handler.hpp
@@ -109,9 +109,22 @@ class request_t final
 		//! Get connection id.
 		connection_id_t connection_id() const noexcept { return m_connection_id; }
 
+		//! Get the remote endpoint.
+		/*!
+		 * \note
+		 * Can throw.
+		 */
+		asio_ns::ip::tcp::endpoint
+		remote_endpoint() const
+		{
+			check_connection();
+
+			return m_connection->remote_endpoint();
+		}
+
 	private:
 		void
-		check_connection()
+		check_connection() const
 		{
 			if( !m_connection )
 			{
