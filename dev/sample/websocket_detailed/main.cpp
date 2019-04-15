2afef120232cc90f288ecc0a956dd7784534397f
eao197 <eao197@gmail.com>
2019-04-15 09:34:26 +0300

    Updated version of websocket_detailed example.
    --HG--
    branch : v.0.4.8.7-update-examples

--- dev/sample/websocket_detailed/main.cpp
+++ dev/sample/websocket_detailed/main.cpp
@@ -26,17 +26,30 @@ class actual_handler_t;
 
 using actual_handler_shptr_t = std::shared_ptr< actual_handler_t >;
 
+// Type of registry with actual handlers.
 using ws_registry_t = std::map< std::uint64_t, actual_handler_shptr_t >;
 
+// The definition of the actual websocket handler.
+//
+// NOTE: this handler is not thread-safe and should be used
+// only when RESTinio is run on a single thread!
+//
 class actual_handler_t final
 	: public std::enable_shared_from_this< actual_handler_t >
 {
 private :
+	// This timer will be used for checking activity of client.
 	asio::steady_timer m_timer;
+
+	// Reference to the registry.
+	// It is necessary to remove handler from it when close_frame is received.
 	ws_registry_t & m_registry;
 
 	// Will be set manually after successful upgrade.
 	rws::ws_handle_t m_wsh;
+
+	// Timestamp of last activity from a client.
+	std::chrono::steady_clock::time_point m_last_activity_at;
 
 	rws::opcode_t m_continued_frame_opcode;
 	std::string m_continuation_frame_buffer;
@@ -60,11 +73,16 @@ public :
 		m_wsh = std::move(wsh);
 		activate( *m_wsh );
 
+		m_last_activity_at = std::chrono::steady_clock::now();
 		schedule_next_ping();
 	}
 
 	void on_message( rws::ws_handle_t wsh, rws::message_handle_t m )
 	{
+		// Timestamp of last client activity can be updated.
+		m_last_activity_at = std::chrono::steady_clock::now();
+		reschedule_timer();
+
 		if( m->opcode() == rws::opcode_t::continuation_frame )
 		{
 			m_continuation_frame_buffer.append( m->payload() );
@@ -112,22 +130,50 @@ public :
 	}
 
 private :
+	const std::chrono::steady_clock::duration m_ping_pause{ 5s };
 
 	void schedule_next_ping()
 	{
-		m_timer.expires_after( 5s );
+		m_timer.expires_after( m_ping_pause );
 		m_timer.async_wait( [this]( auto ec ) {
 				if( ec ) return;
 
-				// Send new ping message.
-				m_wsh->send_message(
-						rws::final_frame,
-						rws::opcode_t::ping_frame,
-						restinio::writable_item_t{} );
+				const auto kill_deadline = m_last_activity_at + 2 * m_ping_pause;
+				const auto now = std::chrono::steady_clock::now();
+				
+				if( now >= kill_deadline )
+				{
+					// It seems that client is dead.
+					std::cout << this << ": client seems to be dead" << std::endl;
 
-				// Restart the timer.
-				schedule_next_ping();
+					// We can kill the connection.
+					const auto id = m_wsh->connection_id();
+					m_wsh->kill();
+					m_registry.erase( id );
+				}
+				else
+				{
+					// A new ping message should be sent.
+					std::cout << this << ": have to send a ping to client" << std::endl;
+
+					// Send new ping message.
+					m_wsh->send_message(
+							rws::final_frame,
+							rws::opcode_t::ping_frame,
+							restinio::writable_item_t{} );
+
+					// Restart the timer.
+					schedule_next_ping();
+				}
 			} );
+	}
+
+	void reschedule_timer()
+	{
+		// Old timer is no more needed.
+		m_timer.cancel();
+		// New timer should be scheduled.
+		schedule_next_ping();
 	}
 };
 
@@ -160,13 +206,20 @@ auto server_handler( asio::io_context & io_ctx, ws_registry_t & registry )
 				auto handler = std::make_shared< actual_handler_t >(
 						io_ctx, registry );
 
+				// Create a websocket in inactive state.
+				// It will be activated manually later, after modification
+				// of the registry.
 				auto wsh = rws::upgrade< traits_t >(
 						*req,
 						rws::activation_t::delayed,
 						handler_proxy_t{ *handler } );
 
+				// The new websocket should be added to the registry.
+				// Otherwise the websocket will be automatically destroyed
+				// on exit out of scope.
 				registry.emplace( wsh->connection_id(), handler );
 
+				// Activate the new websocket.
 				handler->start( wsh );
 
 				return restinio::request_accepted();
@@ -184,6 +237,8 @@ int main()
 
 	try
 	{
+		// We have to use external Asio's io_context because
+		// we should have a reference to it in actual_handler_t.
 		asio::io_context io_ctx;
 
 		ws_registry_t registry;
