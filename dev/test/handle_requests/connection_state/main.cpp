65dea50ff5a620e8519a8983a7eb8e3fbaf0667e
Yauheni Akhotnikau <eao197@gmail.com>
2019-08-19 13:26:09 +0300

    Just a backup commit.
    Check for an exception from state_listener on upgrade to WS added.
    
    --HG--
    branch : 0.6-dev--state-listener-refactoring

--- dev/test/handle_requests/connection_state/main.cpp
+++ dev/test/handle_requests/connection_state/main.cpp
@@ -117,6 +117,42 @@ struct state_listener_that_throws_on_close_t
 	}
 };
 
+struct state_listener_that_throws_on_ws_upgrade_t
+{
+	std::atomic< int > m_accepted{ 0 };
+	std::atomic< int > m_closed{ 0 };
+	std::atomic< int > m_upgraded_to_websocket{ 0 };
+
+	struct cause_visitor_t {
+		state_listener_that_throws_on_ws_upgrade_t & m_self;
+
+		void operator()(
+			const restinio::connection_state::accepted_t & ) const noexcept
+		{
+			++m_self.m_accepted;
+		}
+
+		void operator()(
+			const restinio::connection_state::closed_t & ) const noexcept
+		{
+			++m_self.m_closed;
+		}
+
+		void operator()(
+			const restinio::connection_state::upgraded_to_websocket_t & ) const
+		{
+			++m_self.m_upgraded_to_websocket;
+			throw std::runtime_error( "Something wrong on close!" );
+		}
+	};
+
+	void state_changed(
+		const restinio::connection_state::notice_t & notice )
+	{
+		restinio::visit( cause_visitor_t{ *this }, notice.cause() );
+	}
+};
+
 TEST_CASE( "no connection state listener" , "[no_listener]" )
 {
 	struct test_traits : public restinio::traits_t<
@@ -430,5 +466,87 @@ TEST_CASE( "listener throws on close" , "[throws_on_close]" )
 	REQUIRE( 1 == state_listener->m_accepted.load() );
 	REQUIRE( 1 == state_listener->m_closed.load() );
 	REQUIRE( 0 == state_listener->m_upgraded_to_websocket.load() );
+}
+
+TEST_CASE( "listener throws on WS-upgrade" , "[throws_on_ws_upgrade]" )
+{
+	std::string endpoint_value;
+	bool ws_upgrade_failed = false;
+
+	struct test_traits : public restinio::traits_t<
+			restinio::asio_timer_manager_t,
+			utest_logger_t >
+	{
+		using connection_state_listener_t = state_listener_that_throws_on_ws_upgrade_t;
+	};
+
+	using http_server_t = restinio::http_server_t< test_traits >; 
+
+	auto state_listener = std::make_shared<
+			state_listener_that_throws_on_ws_upgrade_t >();
+
+	http_server_t http_server{
+		restinio::own_io_context(),
+		[&endpoint_value, &ws_upgrade_failed, state_listener]( auto & settings ){
+			settings
+				.port( utest_default_port() )
+				.address( "127.0.0.1" )
+				.connection_state_listener( state_listener )
+				.request_handler(
+					[&endpoint_value, &ws_upgrade_failed]( auto req ){
+						endpoint_value = fmt::format( "{}", req->remote_endpoint() );
+
+						if( restinio::http_connection_header_t::upgrade == req->header().connection() )
+						{
+							try
+							{
+								namespace rws = restinio::websocket::basic;
+								auto ws =
+									rws::upgrade< test_traits >(
+										*req,
+										rws::activation_t::immediate,
+										[]( rws::ws_handle_t,
+											rws::message_handle_t ){} );
+
+								// TODO: write close-message.
+								ws->kill();
+								return restinio::request_accepted();
+							}
+							catch( const std::exception & ex )
+							{
+								std::cout << "UPGRADE FAILED: "
+									<< ex.what() << std::endl;
+								ws_upgrade_failed = true;
+							}
+						}
+						return restinio::request_rejected();
+					} );
+		} };
+
+	other_work_thread_for_server_t<http_server_t> other_thread(http_server);
+	other_thread.run();
+
+	const char * request_str =
+		"GET /chat HTTP/1.1\r\n"
+		"Host: 127.0.0.1\r\n"
+		"Upgrade: websocket\r\n"
+		"Connection: Upgrade\r\n"
+		"Sec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==\r\n"
+		"Sec-WebSocket-Protocol: chat\r\n"
+		"Sec-WebSocket-Version: 1\r\n"
+		"User-Agent: unit-test\r\n"
+		"\r\n";
+
+
+	REQUIRE_THROWS( do_request( request_str ) );
+
+	other_thread.stop_and_join();
+
+	REQUIRE( "" != endpoint_value );
+	REQUIRE( ws_upgrade_failed );
+
+	REQUIRE( 1 == state_listener->m_accepted.load() );
+	REQUIRE( 0 == state_listener->m_closed.load() );
+	REQUIRE( 1 == state_listener->m_upgraded_to_websocket.load() );
 }
 
