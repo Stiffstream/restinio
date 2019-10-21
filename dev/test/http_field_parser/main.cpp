40b080062906fc1111775b449b2d93febea44485
Yauheni Akhotnikau <eao197@gmail.com>
2019-10-21 17:49:43 +0300

    Addition of content_type_value_t.

--- dev/test/http_field_parser/main.cpp
+++ dev/test/http_field_parser/main.cpp
@@ -7,6 +7,7 @@
 #include <restinio/helpers/http_field_parser.hpp>
 #include <restinio/helpers/http_field_parsers/cache-control.hpp>
 #include <restinio/helpers/http_field_parsers/media-type.hpp>
+#include <restinio/helpers/http_field_parsers/content-type.hpp>
 
 namespace hfp = restinio::http_field_parser;
 
@@ -1187,6 +1188,67 @@ TEST_CASE( "Media-Type", "[media-type]" )
 			{ "foo"s, "BaZ"s }
 		};
 		REQUIRE( expected == result.second.m_parameters );
+	}
+}
+
+TEST_CASE( "Content-Type", "[media-type][content-type]" )
+{
+	using namespace restinio::http_field_parsers;
+	using namespace std::string_literals;
+
+	{
+		const auto result = content_type_value_t::try_parse(
+				"text/plain" );
+
+		REQUIRE( result.first );
+
+		REQUIRE( "text" == result.second.m_media_type.m_type );
+		REQUIRE( "plain" == result.second.m_media_type.m_subtype );
+		REQUIRE( result.second.m_media_type.m_parameters.empty() );
+	}
+
+	{
+		const auto result = content_type_value_t::try_parse(
+				"TexT/pLAIn" );
+
+		REQUIRE( result.first );
+
+		REQUIRE( "text" == result.second.m_media_type.m_type );
+		REQUIRE( "plain" == result.second.m_media_type.m_subtype );
+		REQUIRE( result.second.m_media_type.m_parameters.empty() );
+	}
+
+	{
+		const auto result = content_type_value_t::try_parse(
+				"text/*; CharSet=utf-8 ;    Alternative-Coding=\"Bla Bla Bla\"" );
+
+		REQUIRE( result.first );
+
+		REQUIRE( "text" == result.second.m_media_type.m_type );
+		REQUIRE( "*" == result.second.m_media_type.m_subtype );
+
+		media_type_value_t::parameter_container_t expected{
+			{ "charset"s, "utf-8"s },
+			{ "alternative-coding"s, "Bla Bla Bla"s }
+		};
+		REQUIRE( expected == result.second.m_media_type.m_parameters );
+	}
+
+	{
+		const auto result = content_type_value_t::try_parse(
+				"*/*;CharSet=utf-8;Alternative-Coding=\"Bla Bla Bla\";foO=BaZ" );
+
+		REQUIRE( result.first );
+
+		REQUIRE( "*" == result.second.m_media_type.m_type );
+		REQUIRE( "*" == result.second.m_media_type.m_subtype );
+
+		media_type_value_t::parameter_container_t expected{
+			{ "charset"s, "utf-8"s },
+			{ "alternative-coding"s, "Bla Bla Bla"s },
+			{ "foo"s, "BaZ"s }
+		};
+		REQUIRE( expected == result.second.m_media_type.m_parameters );
 	}
 }
 
