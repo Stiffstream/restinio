5ea3abd8a594169e9263240fd24872eb33acd20f
Yauheni Akhotnikau <eao197@gmail.com>
2019-11-28 16:16:27 +0300

    Helper for Accept-Encoding HTTP-field added.

---
+++ dev/restinio/helpers/http_field_parsers/accept-encoding.hpp
@@ -0,0 +1,88 @@
+/*
+ * RESTinio
+ */
+
+/*!
+ * @file
+ * @brief Stuff related to value of Accept HTTP-field.
+ *
+ * @since v.0.6.2
+ */
+
+#pragma once
+
+#include <restinio/helpers/http_field_parsers/media-type.hpp>
+
+namespace restinio
+{
+
+namespace http_field_parsers
+{
+
+//
+// accept_encoding_value_t
+//
+/*!
+ * @brief Tools for working with the value of Accept-Encoding HTTP-field.
+ *
+ * This struct represents parsed value of HTTP-field Accept-Encoding
+ * (see https://tools.ietf.org/html/rfc7231#section-5.3.4):
+@verbatim
+Accept-Encoding  = #( codings [ weight ] )
+codings          = content-coding / "identity" / "*"
+content-coding   = token
+@endverbatim
+ *
+ * @note
+ * Values of `condings` are converted to lower case during the parsing.
+ *
+ * @since v.0.6.2
+ */
+struct accept_encoding_value_t
+{
+	struct item_t
+	{
+		std::string content_coding;
+		restinio::optional_t< qvalue_t > weight;
+	};
+
+	using item_container_t = std::vector< item_t >;
+
+	item_container_t codings;
+
+	/*!
+	 * @brief A factory function for a parser of Accept value.
+	 *
+	 * @since v.0.6.2
+	 */
+	RESTINIO_NODISCARD
+	static auto
+	make_parser()
+	{
+		return produce< accept_encoding_value_t >(
+			maybe_empty_comma_separated_list_producer< item_container_t >(
+				produce< item_t >(
+					token_producer() >> to_lower() >> &item_t::content_coding,
+					maybe( weight_producer() >> &item_t::weight )
+				)
+			) >> &accept_encoding_value_t::codings
+		);
+	}
+
+	/*!
+	 * @brief An attempt to parse Accept HTTP-field.
+	 *
+	 * @since v.0.6.1
+	 */
+	RESTINIO_NODISCARD
+	static expected_t< accept_encoding_value_t, restinio::easy_parser::parse_error_t >
+	try_parse( string_view_t what )
+	{
+		return restinio::easy_parser::try_parse( what, make_parser() );
+	}
+};
+
+} /* namespace http_field_parsers */
+
+} /* namespace restinio */
+
---
+++ dev/test/http_field_parser/accept-encoding.cpp
@@ -0,0 +1,127 @@
+
+/*
+	restinio
+*/
+
+#include <catch2/catch.hpp>
+
+#include <restinio/helpers/http_field_parsers/accept-encoding.hpp>
+
+namespace restinio
+{
+
+namespace http_field_parsers
+{
+
+bool
+operator==( const accept_encoding_value_t::item_t & a,
+	const accept_encoding_value_t::item_t & b ) noexcept
+{
+	return std::tie( a.content_coding, a.weight ) ==
+			std::tie( b.content_coding, b.weight );
+}
+
+} /* namespace http_field_parsers */
+
+} /* namespace restinio */
+
+TEST_CASE( "Accept-Encoding", "[accept-encoding]" )
+{
+	using namespace restinio::http_field_parsers;
+	using namespace std::string_literals;
+
+	{
+		const auto result = accept_encoding_value_t::try_parse(
+				"" );
+
+		REQUIRE( result );
+
+		REQUIRE( result->codings.empty() );
+	}
+
+	{
+		const auto result = accept_encoding_value_t::try_parse(
+				"compress" );
+
+		REQUIRE( result );
+
+		accept_encoding_value_t::item_container_t expected{
+			{ "compress"s, restinio::nullopt }
+		};
+
+		REQUIRE( expected == result->codings );
+	}
+
+	{
+		const auto result = accept_encoding_value_t::try_parse(
+				"compress, gzip" );
+
+		REQUIRE( result );
+
+		accept_encoding_value_t::item_container_t expected{
+			{ "compress"s, restinio::nullopt }
+			, { "gzip"s, restinio::nullopt }
+		};
+
+		REQUIRE( expected == result->codings );
+	}
+
+	{
+		const auto result = accept_encoding_value_t::try_parse(
+				"compress ; q=0.5  , gzip" );
+
+		REQUIRE( result );
+
+		accept_encoding_value_t::item_container_t expected{
+			{ "compress"s, qvalue_t{ qvalue_t::trusted{500} } }
+			, { "gzip"s, restinio::nullopt }
+		};
+
+		REQUIRE( expected == result->codings );
+	}
+
+	{
+		const auto result = accept_encoding_value_t::try_parse(
+				"compress;q=0.5, gzip;q=1" );
+
+		REQUIRE( result );
+
+		accept_encoding_value_t::item_container_t expected{
+			{ "compress"s, qvalue_t{ qvalue_t::trusted{500} } }
+			, { "gzip"s, qvalue_t{ qvalue_t::maximum } }
+		};
+
+		REQUIRE( expected == result->codings );
+	}
+
+	{
+		const auto result = accept_encoding_value_t::try_parse(
+				"gzip;q=1.0, identity; q=0.5, *;q=0" );
+
+		REQUIRE( result );
+
+		accept_encoding_value_t::item_container_t expected{
+			{ "gzip"s, qvalue_t{ qvalue_t::maximum } }
+			, { "identity"s, qvalue_t{ qvalue_t::trusted{500} } }
+			, { "*"s, qvalue_t{ qvalue_t::zero } }
+		};
+
+		REQUIRE( expected == result->codings );
+	}
+
+	{
+		const auto result = accept_encoding_value_t::try_parse(
+				"gzip;q=1.0, identity, *;q=0" );
+
+		REQUIRE( result );
+
+		accept_encoding_value_t::item_container_t expected{
+			{ "gzip"s, qvalue_t{ qvalue_t::maximum } }
+			, { "identity"s, restinio::nullopt }
+			, { "*"s, qvalue_t{ qvalue_t::zero } }
+		};
+
+		REQUIRE( expected == result->codings );
+	}
+}
+
