a8725ba8ef280ea15213490a8aa3d44b0c6f00ad
Yauheni Akhotnikau <eao197@gmail.com>
2019-12-09 18:28:55 +0300

    More new Doxygen comments for Range HTTP-field.

--- dev/restinio/helpers/http_field_parsers/range.hpp
+++ dev/restinio/helpers/http_field_parsers/range.hpp
@@ -261,17 +261,169 @@ other-ranges-specifier = other-range-unit "=" other-range-set
 other-range-set = 1*VCHAR
 @endverbatim
  *
+ * \tparam T integer type for holding parsed values for byte-ranges-specifier.
+ * It is expected to be type like int, unsigned int, long, unsigned long,
+ * std::uint64_t, std::uint_least64_t and so on.
+ *
  * @since v.0.6.2
  */
 template< typename T >
 struct range_value_t
 {
+	/*!
+	 * @brief Value of range for the case where both ends of the range
+	 * are defined.
+	 *
+	 * This type will be used if a range is defined such way:
+	@verbatim
+	bytes=1000-5000,6000-7000
+	@endverbatim
+	 *
+	 * Usage example:
+	 * @code
+		using range_type = restinio::http_field_parsers::range_value_t<std::uint_least64_t>;
+		const auto parse_result = range_type::try_parse(range_field_value);
+		if(parse_result) {
+			if(const auto * byte_ranges =
+					restinio::get_if<range_type::byte_ranges_specifier_t>(parse_result->value)) {
+				for(const auto & r : byte_ranges->ranges) {
+					if(const auto * full_range =
+							restinio::get_if<range_type::double_ended_range_t>(&r)) {
+						... // access to full_range->first and full_range->last
+					}
+					else
+						...
+				}
+			}
+		}
+	 * @endcode
+	 *
+	 * @since v.0.6.2
+	 */
 	using double_ended_range_t = range_details::double_ended_range_t<T>;
+
+	/*!
+	 * @brief Value of range for the case where only left border of the
+	 * range is defined.
+	 *
+	 * This type will be used if a range is defined such way:
+	@verbatim
+	bytes=1000-
+	@endverbatim
+	 *
+	 * Usage example:
+	 * @code
+		using range_type = restinio::http_field_parsers::range_value_t<std::uint_least64_t>;
+		const auto parse_result = range_type::try_parse(range_field_value);
+		if(parse_result) {
+			if(const auto * byte_ranges =
+					restinio::get_if<range_type::byte_ranges_specifier_t>(parse_result->value)) {
+				for(const auto & r : byte_ranges->ranges) {
+					if(const auto * open_range =
+							restinio::get_if<range_type::open_ended_range_t>(&r)) {
+						... // access to open_range->first.
+					}
+					else
+						...
+				}
+			}
+		}
+	 * @endcode
+	 * @since v.0.6.2
+	 */
 	using open_ended_range_t = range_details::open_ended_range_t<T>;
+
+	/*!
+	 * @brief Value of range for the case where only length of range's
+	 * suffix is defined.
+	 *
+	 * This type will be used if a range is defined such way:
+	@verbatim
+	bytes=-450
+	@endverbatim
+	 *
+	 * Usage example:
+	 * @code
+		using range_type = restinio::http_field_parsers::range_value_t<std::uint_least64_t>;
+		const auto parse_result = range_type::try_parse(range_field_value);
+		if(parse_result) {
+			if(const auto * byte_ranges =
+					restinio::get_if<range_type::byte_ranges_specifier_t>(parse_result->value)) {
+				for(const auto & r : byte_ranges->ranges) {
+					if(const auto * suffix =
+							restinio::get_if<range_type::suffix_length_t>(&r)) {
+						... // access to suffix->first.
+					}
+					else
+						...
+				}
+			}
+		}
+	 * @endcode
+	 *
+	 * @since v.0.6.2
+	 */
 	using suffix_length_t = range_details::suffix_length_t<T>;
+
+	/*!
+	 * @brief Variant type for all possible cases of specification for one range.
+	 *
+	 * @since v.0.6.2
+	 */
 	using byte_range_spec_t = range_details::byte_range_spec_t<T>;
+
+	/*!
+	 * @brief A struct that holds a container of byte_range_specs.
+	 *
+	 * Usage example:
+	 * @code
+		using range_type = restinio::http_field_parsers::range_value_t<std::uint_least64_t>;
+		const auto parse_result = range_type::try_parse(range_field_value);
+		if(parse_result) {
+			if(const auto * byte_ranges =
+					restinio::get_if<range_type::byte_ranges_specifier_t>(parse_result->value)) {
+				for(const auto & r : byte_ranges->ranges) {
+					if(const auto * full_range =
+							restinio::get_if<range_type::double_ended_range_t>(&r)) {
+						... // access to full_range->first and full_range->last
+					}
+					else if(const auto * open_range =
+							restinio::get_if<range_type::open_ended_range_t>(&r)) {
+						... // access to open_range->first.
+					}
+					else if(const auto * suffix =
+							restinio::get_if<range_type::suffix_length_t>(&r)) {
+						... // access to suffix->first.
+					}
+				}
+			}
+		}
+	 * @endcode
+	 *
+	 * @since v.0.6.2
+	 */
 	using byte_ranges_specifier_t = range_details::byte_ranges_specifier_t<T>;
+
+	/*!
+	 * @brief A description of a range value of units those are not "bytes".
+	 *
+	 * This type will be used for values like:
+	@verbatim
+	x-megabytes=1-45,450-1300
+	@endverbatim
+	 *
+	 * Please note that other_ranges_specifier_t::range_set contains the raw
+	 * value. E.g. for the example above range_set will hold "1-45,450-1300".
+	 *
+	 * @since v.0.6.2
+	 */
 	using other_ranges_specifier_t = range_details::other_ranges_specifier_t;
+
+	/*!
+	 * @brief Variant type for holding parsed value of Range HTTP-field.
+	 *
+	 * @since v.0.6.2
+	 */
 	using value_t = range_details::value_t<T>;
 
 	value_t value;
