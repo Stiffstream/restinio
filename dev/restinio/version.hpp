bf475b8e309f483e89d8f0a736d693266aa71096
Yauheni Akhotnikau <eao197@gmail.com>
2020-06-23 08:44:29 +0300

    Version number is changed to 0.6.9.

--- dev/restinio/version.hpp
+++ dev/restinio/version.hpp
@@ -11,7 +11,7 @@
 
 #pragma once
 
-// The current version is 0.6.8
+// The current version is 0.6.9
 //
 /*!
  * The major part of version number.
@@ -33,7 +33,7 @@
  *
  * If RESTinio's version is 0.6.23 then RESTINIO_VERSION_PATCH==23.
  */
-#define RESTINIO_VERSION_PATCH 8ull
+#define RESTINIO_VERSION_PATCH 9ull
 
 /*!
  * Helper macro for make single number representation of RESTinio's version.
