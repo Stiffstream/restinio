6bc17fd20ae42b6cac7f1dd02b56b58af1cd6438
Yauheni Akhotnikau <eao197@gmail.com>
2022-07-11 11:27:26 +0300

    Version bump to 0.6.16.

--- dev/restinio/version.hpp
+++ dev/restinio/version.hpp
@@ -11,7 +11,7 @@
 
 #pragma once
 
-// The current version is 0.6.15
+// The current version is 0.6.16
 //
 /*!
  * The major part of version number.
@@ -33,7 +33,7 @@
  *
  * If RESTinio's version is 0.6.23 then RESTINIO_VERSION_PATCH==23.
  */
-#define RESTINIO_VERSION_PATCH 15ull
+#define RESTINIO_VERSION_PATCH 16ull
 
 /*!
  * Helper macro for make single number representation of RESTinio's version.
