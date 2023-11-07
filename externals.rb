ecd2a8991f9fb51e8dad32a0904d3fc35586c365
ngrodzitski <utromvecherom@gmail.com>
2023-11-07 13:17:29 +0100

    Add CI builds with Boost.ASIO

--- externals.rb
+++ externals.rb
@@ -1,5 +1,5 @@
 MxxRu::arch_externals :so5 do |e|
-  e.url 'https://github.com/Stiffstream/sobjectizer/archive/v.5.8.1.tar.gz'
+  e.url 'https://github.com/Stiffstream/sobjectizer/archive/v.5.7.5.tar.gz'
 
   e.map_dir 'dev/so_5' => 'dev'
 end
